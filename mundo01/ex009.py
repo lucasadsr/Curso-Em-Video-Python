50bbf2019dcd64ec8dafdc55822ee8d50730c365
Lucas Araújo <lucasaraujodesa@hotmail.com>
2020-12-28 15:53:39 -0300

    ex009
    Faça um pograma que leia um número inteiro qualquer e mostre na tela a sua tabuada.

---
+++ mundo01/ex009.py
@@ -0,0 +1,15 @@
+# Faça um pograma que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+
+n = int(input('Digite um número para iniciar a tabuada: '))
+print('-' * 12)
+print(f'{n}  X  1 = {n*1}\n'
+      f'{n}  X  2 = {n*2}\n'
+      f'{n}  X  3 = {n*3}\n'
+      f'{n}  X  4 = {n*4}\n'
+      f'{n}  X  5 = {n*5}\n'
+      f'{n}  X  6 = {n*6}\n'
+      f'{n}  X  7 = {n*7}\n'
+      f'{n}  X  8 = {n*8}\n'
+      f'{n}  X  9 = {n*9}\n'
+      f'{n}  X 10 = {n*10}')
+print('-' * 12)
