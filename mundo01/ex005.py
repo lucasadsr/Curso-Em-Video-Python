11a9a635a0df156b9ca83ee211840560e59257fb
Lucas Araújo <lucasaraujodesa@hotmail.com>
2020-12-28 14:14:37 -0300

    ex005
    Faça um programa que leia um número e mostre na tela seu sucessor e antecessor.

---
+++ mundo01/ex005.py
@@ -0,0 +1,5 @@
+# Faça um programa que leia um número e mostre na tela seu sucessor e antecessor. 
+
+n = int(input('digite um número: '))
+print(f'Seu sucessor é: {n+1}\n'
+      f'Seu antecessor é: {n-1}')
