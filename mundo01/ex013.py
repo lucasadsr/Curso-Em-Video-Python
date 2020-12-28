6e50b1d3a2ba4d14c8abd8b6c4db3c23f2b3e288
Lucas Araújo <lucasaraujodesa@hotmail.com>
2020-12-28 16:13:26 -0300

    ex013
    Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.

---
+++ mundo01/ex013.py
@@ -0,0 +1,5 @@
+# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
+sal = float(input('Qual o seu salário? '))
+aumento = (sal * 0.15)
+print(f'Seu novo salário é de: {sal + aumento}')
