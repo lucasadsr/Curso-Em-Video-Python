e598d541a969f8c0d7cf325105cc1695d5e63a0f
Lucas Araújo <lucasaraujodesa@hotmail.com>
2020-12-28 16:22:18 -0300

    ex014
    Escreva um programa que converta uma temperatura digitada em Cº para Fº.

---
+++ mundo01/ex014.py
@@ -0,0 +1,5 @@
+# Escreva um programa que converta uma temperatura digitada em Cº para Fº.
+
+c = float(input('informe a temperatura em °C: '))
+f = 9 * c / 5 + 32
+print(f'A temperaturade {c}°C, corresponde a {f}°F .')
