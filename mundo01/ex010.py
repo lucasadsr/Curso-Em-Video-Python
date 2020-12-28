cf8f3d56f372d57f8caf0a9228fef2afe0f48e21
Lucas Araújo <lucasaraujodesa@hotmail.com>
2020-12-28 15:58:08 -0300

    Create ex010.py
    Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostra quantos dólares ela pode comprar
    Considere o Dólar = 3.27

---
+++ mundo01/ex010.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostra quantos dólares ela pode comprar.
+#Considere o Dólar = 3.27.
+
+v = float(input('Quanto dinheiro você tem na carteira? R$'))
+print(f'Com R${v}, você pode comprar US${v/3.27} !')
