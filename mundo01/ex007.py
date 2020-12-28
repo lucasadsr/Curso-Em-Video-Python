4f090a0adbfac03e11d15f4e0c0a1d743ba810bd
Lucas Araújo <lucasaraujodesa@hotmail.com>
2020-12-28 14:18:43 -0300

    ex007
    Desenvolva um programa que leia duas notas de um aluno, calcule e mostre sua média.

---
+++ mundo01/ex007.py
@@ -0,0 +1,5 @@
+# Desenvolva um programa que leia duas notas de um aluno, calcule e mostre sua média.
+
+n1 = float(input('Digite sua primeira nota: '))
+n2 = float(input('Digite sua segunda nota: '))
+print(f'Sua média é: {(n1+n2)/2:.2f}')
