ff4d13742a52056215603bef424b84798acba6ab
Lucas Araújo <lucasaraujodesa@hotmail.com>
2020-12-28 14:23:30 -0300

    ex008
    Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.

---
+++ mundo01/ex008.py
@@ -0,0 +1,5 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+m = float(input('Digite um valor em metros: '))
+print(f'{m} metro(s) em mm é: {m*1000}\n'
+      f'{m} metro(s) em cm é: {m*100}\n')
