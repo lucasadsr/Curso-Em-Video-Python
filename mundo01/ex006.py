4a833d7dc792d22a8fdc10e6a26d17bcb6e99a5c
Lucas Araújo <lucasaraujodesa@hotmail.com>
2020-12-28 14:16:57 -0300

    ex006
    Crie um algorítmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.

---
+++ mundo01/ex006.py
@@ -0,0 +1,7 @@
+# Crie um algorítmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+n = float(input('digite um número inteiro: '))
+print(f'Seu dobro é: {n*2}\n'
+      f'Seu triplo é: {n*3}\n'
+      f'Sua raiz quadrada é: {n**(1/2)}')
+
