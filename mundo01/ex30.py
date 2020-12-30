dfb4da15b5b65f8f96c8d343b939fe62a69868dd
Lucas Araújo <lucasaraujodesa@hotmail.com>
2020-12-30 17:07:12 -0300

    ex030
    Crie um programa que leia um número inteiro e mostre na tela se ele é par ou ímpar.

---
+++ mundo01/ex30.py
@@ -0,0 +1,7 @@
+'''Crie um programa que leia um número inteiro e mostre na tela se ele é par ou ímpar.'''
+
+n = int(input('Digite um número inteiro: '))
+if n%2 == 0:
+    print('Essse número é par')
+else:
+    print('Esse número é ímpar')
