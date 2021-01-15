5f6a6fa4d4639df82e5493c62881fb44d33aa02e
Lucas Araújo <lucasaraujodesa@hotmail.com>
2021-01-15 15:44:14 -0300

    ex046
    Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifícil, indo de 10 até 0, com pausa de 1 segundo entre eles.

---
+++ mundo02/ex046.py
@@ -0,0 +1,7 @@
+'''Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifícil, indo de 10 até 0, com pausa de 1 segundo entre eles.'''
+
+from time import sleep
+for c in range(10, -1, -1):
+    print(c)
+    sleep(1)
+print('Feliz ano novo!')
