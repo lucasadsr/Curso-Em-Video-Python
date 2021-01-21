156bae4d5b94386eb7b57f86f6039bb70f9218df
Lucas Araújo <lucasaraujodesa@hotmail.com>
2021-01-21 13:24:16 -0300

    ex066
    Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram  digitados e qual foi a soma entre eles (desconciderando o flag).

---
+++ mundo02/ex066.py
@@ -0,0 +1,10 @@
+'''Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram  digitados e qual foi a soma entre eles (desconciderando o flag).'''
+
+soma = total = 0
+while True:
+    num = int(input('Digite um número inteiro (999 para parar): '))
+    if num == 999:
+        break
+    total += 1
+    soma += num
+print(f'Foram digitados {total} números e a soma foi de {soma}.')
